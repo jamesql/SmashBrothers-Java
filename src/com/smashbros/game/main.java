9cb0eeaefb163622e2525749ea6392042021abce
jamesql <thejamesash1@gmail.com>
2021-12-06 11:31:59 -0500

    add map background

--- src/com/smashbros/game/main.java
+++ src/com/smashbros/game/main.java
@@ -30,7 +30,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		new Engine(cfg.get("windowX"), cfg.get("windowY"), primaryStage);
 		
-		Map m = new Map();
+		Map m = new Map("default");
 		m.addPlatformRow(280, 655, 12);
 		m.addPlatformRow(285, 450, 3);
 	}
--- src/com/smashbros/objects/Map.java
+++ src/com/smashbros/objects/Map.java
@@ -2,16 +2,39 @@ package com.smashbros.objects;
 
 import java.util.ArrayList;
 
+import com.smashbros.engine.Config;
 import com.smashbros.engine.Engine;
+import com.smashbros.engine.Overlay;
 
-public class Map {
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Map extends Overlay {
 	private ArrayList<Platform> rows = new ArrayList<Platform>();
+	private String ln;
+	private Image i;
+	private ImageView bg;
+	private Config cfg = Config.instance();
 	
-	public Map() {
+	public Map(String levelName) {
+		super(levelName);
+		this.ln = levelName;
+		this.i = Engine.readImage(String.format("map-%s.png", levelName));
+		this.bg = new ImageView(i);
+		bg.maxHeight(cfg.get("windowY"));
+		bg.setFitWidth(cfg.get("windowX"));
+		spriteList.add(this.bg);
+		this.addNodesToEngine();
+		bg.toBack();
 
 	}
 	
 	public void addPlatformRow(int x, int y, int blocks) {
 		rows.add(new Platform(x, y, blocks));
+	}
+
+	@Override
+	public void render() {
+		// unused
 	}
 }
